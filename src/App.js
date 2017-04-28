26802f261f8c2ea794afed9af882dc637a61d52c
Main_Uddin <mdmain74@gmail.com>
2017-04-28 18:49:14 +0600

    created working prototype

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './App.css'
+import Space from './Space'
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <Space />
+      </div>
+    )
+  }
+}
+
+export default App
---
+++ src/Space.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+class Space extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: []
+    }
+  }
+  render () {
+    return (
+      <div>
+        {this.state.name}
+      </div>
+    )
+  }
+  componentDidMount () {
+    window.fetch('http://api.open-notify.org/astros.json')
+      .then(meta => meta.json())
+      .then(({people}) => {
+        const nameList = people.map((person, idx) => <li key={idx}>{person.name}</li>)
+        this.setState({name: nameList})
+      })
+  }
+}
+export default Space
